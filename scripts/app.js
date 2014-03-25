705ad12137df3ba70cfa6f8f43ef944f7ddeb2f0
Emilio J. Diez Fraile <ejdiezfraile@gmail.com>
2014-03-25 22:44:21 +0100

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+/* global app:true */
+'use strict';
+
+var app = angular.module('angNewsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'firebase'
+]);
+
+app.constant('FIREBASE_URL', 'https://shining-fire-7278.firebaseio.com/');
+
+app.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/posts.html',
+    controller: 'PostsCtrl',
+  })
+  .when('/posts/:postId', {
+		templateUrl: 'views/showpost.html',
+		controller: 'PostViewCtrl'
+	})
+  .when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'AuthCtrl'
+  })
+  .when('/register', {
+    templateUrl: 'views/register.html',
+    controller: 'AuthCtrl'
+  })
+  .when('/users/:username', {
+    templateUrl: 'views/profile.html',
+    controller: 'ProfileCtrl'
+  })
+	.otherwise({
+		redirectTo: '/'
+  });
+});
---
+++ scripts/controllers/posts.js
@@ -0,0 +1,11 @@
+'use strict';
+ 
+app.controller('PostsCtrl', function ($scope, $location, Post) {
+  if ($location.path() === '/') {
+    $scope.posts = Post.all;
+  }
+
+	$scope.deletePost = function (postId) {
+		Post.delete(postId);
+	};
+});
